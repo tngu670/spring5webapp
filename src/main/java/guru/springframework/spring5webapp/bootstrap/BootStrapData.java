9833e741c54b5f26296bf4ad00ffcf79f9eed141
Taylor Nguyen <tngu670@wgu.edu>
2024-04-24 19:28:00 -0700

    Added Publisher and PublisherRepository

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,14 +14,23 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        Publisher publisher = new Publisher(
+                "Nguyen Publishing", "31415", "Pi Lane", "CA", "90210");
+
+        publisherRepository.save(publisher);
+
+        System.out.println("Publisher Count: " + publisherRepository.count());
 
         Author taylor = new Author("Taylor", "Nguyen");
         Book helloWorld = new Book("Hello World", "123123");
